8150455cabaefbdd11e22253aa7988ad0295e94f
romphroy <romphroy@gmail.com>
2023-03-21 15:38:11 -0700

    Implement Google autofill address functionality.

---
+++ accounts/context_processors.py
@@ -0,0 +1,14 @@
+from vendor.models import Vendor
+from django.conf import settings
+
+def get_vendor(request):
+    try: 
+        vendor = Vendor.objects.get(user=request.user)
+    except:
+        vendor = None
+    return dict(vendor=vendor)
+
+
+# Google Map API Configuration
+def get_google_api(request):
+    return {'GOOGLE_API_KEY': settings.GOOGLE_API_KEY}
